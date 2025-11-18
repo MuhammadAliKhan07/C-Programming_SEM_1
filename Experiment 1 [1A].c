083ef1b1617ad683b8ad19d81bf05690003f5c47
MuhammadAliKhan07 <muhammadalik730@gmail.com>
2025-11-18 21:04:08 +0530

    Insert author information in header comments
    Added header comments to include author details.

---
+++ Experiment 1 [1A].c
@@ -0,0 +1,18 @@
+/*
+Name: Muhammad Ali Muhammad Yusuf Khan
+UIN: 251A028
+Class: FE AI & DS
+Div: A
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Your name is Muhammad Ali Muhammad Yusuf Khan\n");
+    printf("Age: 18 yrs\n");
+    printf("Class: FE AI & DS\n");
+    printf("Division: A\n");
+    printf("UIN: 251A028\n");
+
+    return 0;
+}
